c948dfdb6255edb65d13bff28046baf2b12585a1
Dan Trickey <dan@trickey.io>
2018-07-28 17:01:29 +0100

    Got Command Handler working

--- src/firmware.cpp
+++ src/firmware.cpp
@@ -15,6 +15,123 @@ static const CommandError OK = "";
 
 static Adafruit_PWMServoDriver SERVOS = Adafruit_PWMServoDriver();
 
+class CommandHandler {
+  public:
+    String command;
+    CommandError (*run)(int, String argument);
+
+    CommandHandler(String cmd, CommandError(*runner)(int, String));
+};
+
+CommandHandler::CommandHandler(String cmd, CommandError (*runner)(int, String))
+: command(cmd), run(runner)
+{
+  
+}
+
+static const CommandHandler commands[] = {
+  
+};
+
+static void serialWrite(int commandId, char lineType, const String& str) {
+    if (commandId != 0) {
+        Serial.write('@');
+        Serial.print(commandId, DEC);
+        Serial.write(' ');
+    }
+
+    Serial.write(lineType);
+    Serial.write(' ');
+
+    Serial.println(str);
+}
+
+static void dispatch_command(int commandId, const class CommandHandler& handler, const String& argument) {
+  auto err = handler.run(commandId, argument);
+  if (err == OK) {
+    serialWrite(commandId, '+', "OK");
+  } else {
+    serialWrite(commandId, '-', String("Error: ") + err);
+  }
+}
+
+static void handle_actual_command(int commandId, const String& cmd) {
+    for (int i = 0; i < sizeof(commands) / sizeof(CommandHandler); ++i) {
+      const CommandHandler& handler = commands[i];
+
+      if (handler.command == cmd) {
+        dispatch_command(commandId, handler, "");
+        return;
+      } else if (cmd.startsWith(handler.command + " ")) {
+        dispatch_command(
+          commandId,
+          handler,
+          cmd.substring(handler.command.length() + 1)
+        );
+        return;
+      }
+    }
+
+    serialWrite(commandId, '-', String("Error, unknown command: ") + cmd);
+}
+
+static void handle_command(const String& cmd) {
+  // A commans is prefixed with @ if an identifier is used to prevent race conditions
+  if (cmd.startsWith("@")) {
+    auto spaceIndex = cmd.indexOf(' ');
+    auto commandId = cmd.substring(1, spaceIndex).toInt();
+    handle_actual_command(commandId, cmd.substring(spaceIndex + 1));
+  } else {
+    handle_actual_command(0, cmd);
+  }
+}
+
+static String serialBuffer;
+static boolean skipWS = false;
+
+static void process_serial() {
+  auto serialInput = Serial.read();
+
+  // Allow resetting the buffer by sending a NULL. This allows for recovery
+  // from partial commands being sent or received.
+  if (serialInput == 0) {
+    serialBuffer = "";
+    return;
+  }
+
+  if (serialInput == -1) {
+    return;
+  }
+
+  if (serialInput == '\r') {
+    return; // ignore CR, just take the LF
+  }
+
+  if (serialInput == '\t') {
+    serialInput = ' '; // treat tabs as equivalent to spaces
+  }
+
+  if (serialInput == '\n') {
+    serialBuffer.trim();
+    Serial.write("# ");
+    Serial.write(serialBuffer.c_str());
+    Serial.write('\n');
+    handle_command(serialBuffer);
+    serialBuffer = "";
+    Serial.flush();
+    return;
+  }
+
+  if (serialInput == ' ' && skipWS) {
+    return; // ignore junk whitespace
+  } else {
+    skipWS = (serialInput == ' '); // ignore any successive whitespace
+  }
+
+  serialBuffer += (char)serialInput;
+}
+
+
 void setup() {
   // Setup the pins.
   pinMode(LED_BUILTIN, OUTPUT);
@@ -33,6 +150,8 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  while (Serial.available()) {
+   process_serial();
+  }
 }
+
